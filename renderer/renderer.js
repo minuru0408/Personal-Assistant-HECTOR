de0b354e31826c6c629461da6cebc4c7371d33e7
Your Name <your@email.com>
2025-07-02 03:07:22 +0900

    fd

---
+++ renderer/renderer.js
@@ -0,0 +1,9 @@
+console.log('Renderer loaded')
+
+// TODO: Add event listeners for command input
+document.querySelector('.command-input').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        // TODO: Handle command processing
+        console.log('Command entered:', e.target.value)
+    }
+})
