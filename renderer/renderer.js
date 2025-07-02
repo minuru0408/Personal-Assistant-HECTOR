8a489067afccdb25ba7480f83703ca9166617f51
Your Name <your@email.com>
2025-07-02 15:15:47 +0900

    fds

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,11 +1,1 @@
-console.log("Dashboard renderer loaded");
-
-// TODO: hook up interactivity
-
-// TODO: Add event listeners for command input
-document.querySelector('.command-input').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        // TODO: Handle command processing
-        console.log('Command entered:', e.target.value)
-    }
-})
+// This file is intentionally empty - no client-side JS needed
