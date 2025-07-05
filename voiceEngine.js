50724948a59743b4a181397a45c22c78f5033609
Your Name <your@email.com>
2025-07-06 01:39:05 +0900

    gfd

--- voiceEngine.js
+++ voiceEngine.js
@@ -62,7 +62,7 @@ async function checkEnd(history) {
   }
 }
 
-function filterNoise(input, output, threshold = 0.01) {
+function filterNoise(input, output, threshold = 0.05) {
   const buf = fs.readFileSync(input);
   if (buf.length <= 44) return false;
   const header = buf.subarray(0, 44);
