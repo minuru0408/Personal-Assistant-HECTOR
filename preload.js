d4e684b6a15734daa48f7fb79fbafb72ef05bb60
MINURU <minuru0408@gmail.com>
2025-07-05 19:49:19 +0900

    Remove howler and use built-in Audio

--- preload.js
+++ preload.js
@@ -1,8 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const { Howl } = require('howler');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  Howl,
   sendMessage: (text) => ipcRenderer.invoke('send-message', text),
   elevenLabsApiKey: process.env.ELEVENLABS_API_KEY
 });
