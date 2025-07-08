01f0e3058ce5d6d6734b6ab188880d51040635a5
MINURU <minuru0408@gmail.com>
2025-07-08 11:06:28 +0900

    Update calendar scope

--- calendar.js
+++ calendar.js
@@ -3,7 +3,7 @@ const path = require('path');
 const readline = require('readline');
 const { google } = require('googleapis');
 
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const CREDENTIALS_PATH = path.join(__dirname, 'calendar-credentials.json');
 const TOKEN_PATH = path.join(__dirname, 'calendar-token.json');
 
