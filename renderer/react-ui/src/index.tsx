bdd194c5b4b6190d2b092d0f8aebc59c3495ca92
Your Name <your@email.com>
2025-07-18 08:26:34 +0800

    ewq

--- renderer/react-ui/src/index.tsx
+++ renderer/react-ui/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Dashboard from './Dashboard';
+import Dashboard from './components/Dashboard';
 import './index.css';
 
 const root = createRoot(document.getElementById('root')!);
